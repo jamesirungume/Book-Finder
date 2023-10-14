77985906bdebd31b7e89086062116cef58d41c5d
mararo-ops <danielmararo1@gmail.com>
2023-10-14 14:59:03 +0300

    final js fetch best books awarded

--- index.js
+++ index.js
@@ -25,3 +25,50 @@ async function searchBooksByName(bookName) {
                 </div>
             `;
         }
+		 // Display the results
+		 document.getElementById('bookNameDisplay').innerHTML = outputHtml;
+
+		} catch (error) {
+			console.error(error);
+		}
+	}
+	
+	document.getElementById('bookNameSearchButton').addEventListener('click', () => {
+		searchBooksByName(document.getElementById('bookNameInput').value);
+	});
+	
+	document.addEventListener('DOMContentLoaded', async () => {
+		const url = 'https://hapi-books.p.rapidapi.com/top/2021';
+		const options = {
+			method: 'GET',
+			headers: {
+				'X-RapidAPI-Key': '6b3542ccbbmsh9b69be6392a288fp1473e1jsn40f76518b8bc',
+				'X-RapidAPI-Host': 'hapi-books.p.rapidapi.com'
+			}
+		};
+	
+		try {
+			const response = await fetch(url, options);
+			const books = await response.json();
+	
+			const bookListDiv = document.getElementById('bookList');
+			
+			let bookHTML = '';
+	
+			for (const book of books) {
+				bookHTML += `
+					<div class="book-item">
+						<img src="${book.cover}" alt="${book.name}">
+						<h2>${book.name}</h2>
+						<p><strong>Category:</strong> ${book.category}</p>
+						<p><a href="${book.url}" target="_blank">Read More</a></p>
+					</div>
+				`;
+			}
+	
+			bookListDiv.innerHTML = bookHTML;
+	
+		} catch (error) {
+			console.error(error);
+		}
+	});
